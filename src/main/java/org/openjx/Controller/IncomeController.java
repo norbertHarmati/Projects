49af56497708212dca5f829e2f1b378d0a93dd35
norbertHarmati <norbert.harmati92@gmail.com>
2020-07-24 18:32:07 +0200

    Continue the implementation of incomeView
    implementations of labels, tooltips and datapicker

--- src/main/java/org/openjx/Controller/IncomeController.java
+++ src/main/java/org/openjx/Controller/IncomeController.java
@@ -1,7 +1,12 @@
 package org.openjx.Controller;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import org.openjx.App;
 import org.openjx.View.IncomeView;
@@ -10,14 +15,34 @@ import org.openjx.View.IncomeView;
 public class IncomeController {
 
     Label back;
-
+    TextArea comment;
+    TextField amount, from_whom_the_income_comes;
+    DatePicker date;
+    private static final int max_length = 45;
 
     public IncomeController() {
         back = (Label) IncomeView.getPane().getChildren().get(0);
+        amount = (TextField) IncomeView.getPane().getChildren().get(2);
+        date = (DatePicker) IncomeView.getPane().getChildren().get(3);
+        from_whom_the_income_comes = (TextField) IncomeView.getPane().getChildren().get(4);
+        comment = (TextArea) IncomeView.getPane().getChildren().get(1);
+        addTextLimiter(comment,max_length);
         addMouseEvent();
         mouseEntered();
         mouseExited();
 
+    }
+
+    public static void addTextLimiter(final TextArea tf, final int maxLength) {
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
+                if (tf.getText().length() > maxLength) {
+                    String s = tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
     }
 
     void addMouseEvent() {
--- src/main/java/org/openjx/View/IncomeView.java
+++ src/main/java/org/openjx/View/IncomeView.java
@@ -1,8 +1,7 @@
 package org.openjx.View;
 
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import org.openjx.App;
 import org.openjx.Controller.IncomeController;
@@ -14,29 +13,116 @@ public class IncomeView {
         return pane;
     }
 
-    private  static Pane pane;
-    Label label;
-    TextField comment;
+    private static Pane pane;
+    Label label, amountL, dateL, from_whom_the_income_comesL, commentL;
+    Tooltip amountT, dateT, from_whom_the_income_comesT, commentT;
+    TextArea comment;
+    TextField amount, from_whom_the_income_comes;
+    DatePicker date;
 
     public IncomeView() {
         init();
         Scene scene = new Scene(pane, App.screen.getBounds().getWidth(), App.screen.getBounds().getHeight());
+        scene.getStylesheets().add("text.css");
         App.setBackground(pane);
         App.setScene(scene);
         new IncomeController();
+    }
+
+    void creatingLabels() {
+        label = new Label("Back");
+        amountL = new Label(" Your income");
+        dateL = new Label("Date");
+        from_whom_the_income_comesL = new Label("Your name");
+        commentL = new Label("Your comment");
+
+    }
+
+    void creatingTooltips() {
+
+        amountT = new Tooltip("");
+        dateT = new Tooltip("");
+        commentT = new Tooltip("Here you can write comments if you wish." +
+                "You have  max. 45 character");
+        from_whom_the_income_comesT = new Tooltip("The name whom the income comes");
+    }
+
+    void setTooltips() {
+
+        amountL.setTooltip(amountT);
+        dateL.setTooltip(dateT);
+        from_whom_the_income_comesL.setTooltip(from_whom_the_income_comesT);
+        commentL.setTooltip(commentT);
 
     }
 
     public void init() {
 
         pane = new Pane();
-        label = new Label("Back");
-        comment = new TextField();
-        pane.getChildren().addAll(label, comment);
-        label.setLayoutX(50);
-        label.setLayoutY(App.screen.getBounds().getHeight() - 50);
-        App.setFonts(label);
+        comment = new TextArea();
+        amount = new TextField();
+        from_whom_the_income_comes = new TextField();
+        date = new DatePicker();
+
+
+        comment.setWrapText(true);
+
+        creatingLabels();
+        creatingTooltips();
+
+        setTooltips();
+        setLayouts();
+
+        setFonts();
+        setPane();
+
+
+
     }
+
+    void setPane(){
+        pane.getChildren().addAll(label, comment, amount, date, from_whom_the_income_comes, commentL, amountL, dateL, from_whom_the_income_comesL);
+    }
+
+    void setLayouts(){
+        App.setLayout(commentL, App.screen.getBounds().getWidth() / 2 + 400, App.screen.getBounds().getHeight() / 2 - 50);
+        App.setLayout(amountL, App.screen.getBounds().getWidth() / 2 + 100, App.screen.getBounds().getHeight() / 2 - 50);
+        App.setLayout(dateL, App.screen.getBounds().getWidth() / 2 - 500, App.screen.getBounds().getHeight() / 2 - 50);
+        App.setLayout(from_whom_the_income_comesL, App.screen.getBounds().getWidth() / 2 - 200, App.screen.getBounds().getHeight() / 2 - 50);
+        App.setLayout(label, 50, App.screen.getBounds().getHeight() - 50);
+
+        date.setLayoutX(App.screen.getBounds().getWidth() / 2 - 500);
+        date.setLayoutY(App.screen.getBounds().getHeight() / 2);
+
+        setTextArea(comment, App.screen.getBounds().getWidth() / 2 + 400, App.screen.getBounds().getHeight() / 2);
+        setTextfield(amount, App.screen.getBounds().getWidth() / 2 + 100, App.screen.getBounds().getHeight() / 2);
+        setTextfield(from_whom_the_income_comes, App.screen.getBounds().getWidth() / 2 - 200, App.screen.getBounds().getHeight() / 2);
+
+    }
+
+    void setFonts(){
+        App.setFonts(label);
+        App.setFonts(commentL);
+        App.setFonts(amountL);
+        App.setFonts(dateL);
+        App.setFonts(from_whom_the_income_comesL);
+
+    }
+
+    public static void setTextfield(TextField textField, double x, double y) {
+
+        textField.setLayoutX(x);
+        textField.setLayoutY(y);
+        textField.setMaxSize(150, 50);
+    }
+
+    public static void setTextArea(TextArea textArea, double x, double y) {
+
+        textArea.setLayoutX(x);
+        textArea.setLayoutY(y);
+        textArea.setMaxSize(150, 50);
+    }
+
 
 
 }
